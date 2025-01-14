1c1697390438f132bf48643c3e89da777fdec3f3
duccini <guilhermeduccini@gmail.com>
2025-01-14 18:35:38 -0300

    feat: add nutrition info

--- src/screens/RecipeDetails/index.jsx
+++ src/screens/RecipeDetails/index.jsx
@@ -1,6 +1,6 @@
 import { memo } from "react";
 
-const { SafeAreaView, Image } = require("react-native");
+const { SafeAreaView, Image, View, Text } = require("react-native");
 
 import Title from "../../components/Title";
 
@@ -8,11 +8,21 @@ import styles from "./styles";
 
 const RecipeDetails = ({ route }) => {
   const { item } = route.params || {};
+  const nutrition = { ...item.nutrition };
+  delete nutrition?.updated_at;
+  const nutritionKeys = Object.keys(nutrition || {}); // array
 
   return (
     <SafeAreaView style={styles.container}>
       <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url }} />
-      <Title text={item?.name} />
+      <Title style={styles.title} text={item?.name} />
+
+      {nutritionKeys.map((key) => (
+        <View style={styles.row} key={key}>
+          <Text style={styles.nutritionTitle}>{key.toUpperCase()}</Text>
+          <Text style={styles.nutritionValue}>{nutrition[key]}</Text>
+        </View>
+      ))}
     </SafeAreaView>
   );
 };
--- src/screens/RecipeDetails/styles.js
+++ src/screens/RecipeDetails/styles.js
@@ -1,5 +1,4 @@
 import { StyleSheet, Dimensions } from "react-native";
-
 import colors from "../../styles/colors";
 
 const { width } = Dimensions.get("window");
@@ -18,6 +17,30 @@ const styles = StyleSheet.create({
     borderRadius: 10,
 
     marginBottom: 24,
+  },
+
+  title: {
+    marginBottom: 12,
+  },
+
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "rgba(212, 217, 217, 0.5)",
+    borderRadius: 8,
+    padding: 8,
+    marginVertical: 4,
+    paddingHorizontal: 12,
+    paddingVertical: 10,
+  },
+
+  nutritionTitle: {
+    textTransform: "capitalize",
+  },
+
+  nutritionValue: {
+    color: "rgba(0,0,0,0.45)",
   },
 });
 
